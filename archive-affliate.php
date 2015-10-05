1685a8c9a0407a0ac9940b661535952182f12f47
Cody Collicott <cody@dashboard.ca>
2015-10-05 10:18:13 -0400

    v1.2

--- archive-affliate.php
+++ archive-affliate.php
@@ -5,21 +5,21 @@
  * @package   plugin
  * @author    Cody Collicott
  * @license   GPL-2.0+
- * @link      https://github.com/dashboard-repo/plugin
+ * @link      https://github.com/dashboard-repo/plugin2
  */
 
 /*
 Plugin Name:       Example Plugin
-Plugin URI:        https://github.com/dashboard-repo/plugin
+Plugin URI:        https://github.com/dashboard-repo/plugin2
 Description:       tester
-Version:           1.0
+Version:           1.1
 Author:            Cody Collicott
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
 Domain Path:       /languages
 Text Domain:       github-updater
 Network:           true
-GitHub Plugin URI: https://github.com/dashboard-repo/plugin
+GitHub Plugin URI: https://github.com/dashboard-repo/plugin2
 GitHub Branch:     master
 Requires WP:       3.8
 Requires PHP:      5.3
